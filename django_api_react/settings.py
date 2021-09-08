967bf6d8f43edad864138b9b4d13a9ca5c2269a9
jjmountain <devereuxjj@gmail.com>
2021-09-08 22:36:17 +0900

    with .env

--- django_api_react/settings.py
+++ django_api_react/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7sqbe0vw4^=e5-6bn4hs7*g4yjmnqd^$oynpkp^5_%371^cf5!'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
